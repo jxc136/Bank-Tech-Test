e577acf36cd5a84a2e88509a4d34d1913ace93f1
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-23 15:04:21 +0000

    add account test outlines for account deposit and withdraw

--- Bank-Tech-Test/tests/account.test.js
+++ Bank-Tech-Test/tests/account.test.js
@@ -1,5 +1,51 @@
 require ('../src/account')
 
 describe ('Account', () => {
+  
+  describe ('deposit', () => {
+
+    // Test 1 - it updates the balance with a small deposit value 
+
+    // Test 2 - it updates the balance with a valid large deposit value 
+
+    // Test 3 - it updates the balance when two valid deposits are called
+
+    // Test 4 - it does not update the balance with an invalid deposit objecy
+
+    // Test 5 - It does not update the balance with a deposit that is too large
+
+    // Test 6 - a valid deposit will be accepted when proceeded by an invalid deposit
+
+  })
+
+  describe ('withdraw', () => {
+
+    // Test 1 - it updates the balance with a valid small withdraw value 
+
+    // Test 2 - it updates the balance with a valid large withdraw value 
+
+    // Test 3 - it updates the balance when two valid Withdrawals are called
+
+    // Test 4 - it does not update the balance with an invalid deposit object
+
+    // Test 5 - it wont update the balance with a valid withdrawal value that exceeds balance 
+
+    // Test 6 - It does not update the balance with a withdrawal that exceeds withdrawal restrictions
+
+    // Test 7 - a valid withdrawwl will be accepted when proceeded by an invalid deposit
+
+  })
+
+  describe ('validateTransaction', () => {
+    
+  })
+
+  describe ('logTransaction', () => {
+    
+  })
+
+  describe ('printStatement', () => {
+    
+  })
 
 })
