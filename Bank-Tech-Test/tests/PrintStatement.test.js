4690887c37c819341c9241d7377ff72951d9bd1d
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-24 17:25:24 +0000

    add test for statement with multiple transactions

--- Bank-Tech-Test/tests/PrintStatement.test.js
+++ Bank-Tech-Test/tests/PrintStatement.test.js
@@ -25,7 +25,19 @@ describe('PrintStatment', () => {
   })
 
 
-  // 2 - It prints a statment with multiple transactions 
+  // 3 - It prints a statment with multiple transactions 
+
+  test('format and print 3 transaction', () => {
+    const log = [{date: "30/11/2022", credit: 0, debit: 333.33, balance: 333.33 },
+    {date: "05/06/2022", credit: 111.11, debit: 0, balance: 222.22 },
+    {date: "07/07/2022", credit: 0, debit: 222.22, balance: 444.44 }]
+    const statement = new PrintStatement(log)
+    const expectedStatement =
+    `date || credit || debit || balance\n30/11/2022 || 0 || 333.33 || 333.33\n05/06/2022 || 111.11 || 0 || 222.22\n07/07/2022 || 0 || 222.22 || 444.44\n`;
+    statement.format();
+    expect(statement.print()).toBe(expectedStatement)
+      
+  })
 
   // 3 - It prints a message if no transactions 
 })
