713fae5ed271b1d908dd85b1aa31b5cd2970eb5c
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-24 22:36:05 +0000

    refactor: move validate method from account class into a new validator class

--- Bank-Tech-Test/src/Validator.js
+++ Bank-Tech-Test/src/Validator.js
@@ -1,4 +1,13 @@
 class Validator {
 
+  validate(transaction){
+    if (typeof((transaction.debit)) === "number" && 
+    typeof((transaction.credit)) === "number" &&
+    transaction.date && transaction.debit <= 1000
+    && transaction.credit <= 1000 ) {
+      return true
+    }
+    else {return false}
+  }
 }
-module.exports = Validator
+module.exports = Validator;
--- Bank-Tech-Test/tests/Validator.test.js
+++ Bank-Tech-Test/tests/Validator.test.js
@@ -1,5 +1,85 @@
-require ('../src/Validator')
+const Validator = require ('../src/Validator')
 
 describe ('Validator', () => {
-  
+    
+    // Test 1 - Valid deposit object
+
+    test('valid deposit object', () => {
+      const validator = new Validator
+      const deposit = {date: "11/12/2021", credit: 0, debit: 10.00}
+      expect(validator.validate(deposit)).toEqual(true)
+    })
+
+    // Test 2 - Invalid deposit object
+
+    test('deposit is not a number', () => {
+      const validator = new Validator
+      const deposit = {date:'02/03/2022', credit: 0, debit: 'string'}
+      expect(validator.validate(deposit)).toEqual(false)
+    })
+
+    test('no date', () => {
+      const validator = new Validator
+      const deposit = {credit: 0, debit: 10.00}
+      expect(validator.validate(deposit)).toEqual(false)
+    })
+
+
+    //  Test 3 - Valid deposit amount
+
+    test('valid deposit amount', () => {
+      const validator = new Validator
+      const deposit = {date: 22/12/2020, credit: 0, debit: 37.22}
+      expect(validator.validate(deposit)).toEqual(true)
+    })
+
+    // Test 4 - Invalid deposit amount
+
+    test('deposit exceeds 1000', () => {
+      const validator = new Validator
+      const deposit = {date: '03/11/2019', credit: 0, debit: 1001.01}
+      expect(validator.validate(deposit)).toEqual(false)
+    })
+
+    // // Test 5 - Valid deposit amount
+
+    test('deposit below 1000', () => {
+      const validator = new Validator
+      const deposit = {date: '05/10/2018', credit: 0, debit: 1000.00}
+      expect(validator.validate(deposit)).toEqual(true)
+    })
+
+    // Test 6 - valid withdraw object
+
+    test('withdraw below 1000', () => {
+      const validator = new Validator
+      const withdraw = {date: '05/10/2018', credit: 999.00, debit: 0}
+      expect(validator.validate(withdraw)).toEqual(true)
+    })
+
+    // // Test 7 - Invalid Withdraw object
+
+    test('withdraw object is not a number', () => {
+      const validator = new Validator
+      const withdraw = {date: '04/11/2018', credit: 'NAN', debit: 0}
+      expect(validator.validate(withdraw)).toEqual(false)
+    })
+
+    // // Test 7 - Valid Withdraw amount
+
+    test('withdraw below 1000', () => {
+      const validator = new Validator 
+      const withdraw = {date: '05/10/2018', credit: 999.00, debit: 0}
+      expect(validator.validate(withdraw)).toEqual(true)
+    })
+
+    // Test 8 - Invalid Withdraw amount
+
+    test('invalid withdraw amount', () => {
+      const validator = new Validator
+      const withdraw = {date: '04/04/2022', credit: 1001.11, debit: 0}
+      expect(validator.validate(withdraw)).toEqual(false)
+    })
+    
+
 })
