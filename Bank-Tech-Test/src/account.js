7eff55366a2e32e66b6d8eedfd4fd17761a44d6c
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-24 23:45:14 +0000

    refactor: fix style issues in Account class

--- Bank-Tech-Test/src/account.js
+++ Bank-Tech-Test/src/account.js
@@ -1,37 +1,30 @@
-const Validator = require ('./Validator')
+const Validator = require ('./Validator');
 
 class Account {
   constructor(validator) {
     this.balance = 0;
-    this.validator = validator
+    this.validator = validator;
   }
   
 
   validate(transaction) {
-    this.validator.validate(transaction)
-    // if (typeof((transaction.debit)) === "number" && 
-    // typeof((transaction.credit)) === "number" &&
-    // transaction.date && transaction.debit <= 1000
-    // && transaction.credit <= 1000 ) {
-    //   return true
-    // }
-    // else {return false}
-  };
+    this.validator.validate(transaction);
+  }
   
   deposit(transaction) {
     
     if (this.validate(transaction) === true){
-      this.balance += transaction.debit
+      this.balance += transaction.debit;
     }
     // need to return an error message otherwise
-  };
+  }
 
   withdraw(transaction) {
 
     if (this.validate(transaction) === true && this.balance >= transaction.credit){
-      this.balance -= transaction.credit
+      this.balance -= transaction.credit;
     }
-  };
+  }
 
  
 }
