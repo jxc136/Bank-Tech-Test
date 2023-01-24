92cddd45eb8cb216bba85b26639081843396121c
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-24 15:55:51 +0000

    add test for multiple transaction is log

--- Bank-Tech-Test/tests/logTransaction.test.js
+++ Bank-Tech-Test/tests/logTransaction.test.js
@@ -28,13 +28,24 @@ describe ('LogTransaction', () => {
   test ( 'Two valid transactions added', () => {
     log = new LogTransaction
     const transaction1 = {date: "11/30/2022", credit: 0, debit: 500.55, balance: 500.55 }
-    const transaction2 = {date: "11/30/2022", credit: 150.50, debit: 500.55, balance: 350.05 }
+    const transaction2 = {date: "11/30/2022", credit: 150.50, debit: 0, balance: 350.05 }
     log.add(transaction1)
     log.add(transaction2)
     expect(log.returnLog()).toEqual(expect.arrayContaining([transaction1, transaction2]))
   })
 
   // Test 4 - Three Transactions 
+
+  test ( 'Two valid transactions added', () => {
+    log = new LogTransaction
+    const transaction1 = {date: "22/04/2019", credit: 0, debit: 500.55, balance: 500.55 }
+    const transaction2 = {date: "25/05/2019", credit: 150.50, debit: 0, balance: 350.05 }
+    const transaction3 = {date: "25/05/2019", credit: 40.00, debit: 0, balance: 310.05 }
+    log.add(transaction1)
+    log.add(transaction2)
+    log.add(transaction3)
+    expect(log.returnLog()).toEqual(expect.arrayContaining([transaction1, transaction2, transaction3]))
+  })
 
   // Test 5 - Different Date Formats 
 
