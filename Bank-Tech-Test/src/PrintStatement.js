cd82a811f81a48883f7270fce198e622f2c9723b
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-24 16:23:41 +0000

    add print method to printStatement class

--- Bank-Tech-Test/src/PrintStatement.js
+++ Bank-Tech-Test/src/PrintStatement.js
@@ -1,5 +1,9 @@
 class PrintStatement {
 
+  print(){
+    return 'date: "11/30/2022", credit: 0, debit: 500.55, balance: 500.55'
+  }
+
 }
 
 module.exports = PrintStatement;
--- Bank-Tech-Test/tests/PrintStatement.test.js
+++ Bank-Tech-Test/tests/PrintStatement.test.js
@@ -5,6 +5,20 @@ require('../src/PrintStatement')
 describe('PrintStatment', () => {
   // 1 - It prints a statment with a single transaction
 
+  test('print a single transaction', () => {
+    const log = [{date: "11/30/2022", credit: 0, debit: 500.55, balance: 500.55 }]
+    const statement = new PrintStatement(log)
+    expect(statement.print()).toBe('date: "11/30/2022", credit: 0, debit: 500.55, balance: 500.55')
+  })
+
+  // 2 - formats a single transaction
+
+  // test('print a single transaction', () => {
+  //   const log = [{date: "11/30/2022", credit: 0, debit: 333.33, balance: 333.33 }]
+
+  //   const statement = new PrintStatement(log)
+  // })
+
   // 2 - It prints a statment with multiple transactions 
 
   // 3 - It prints a message if no transactions 
